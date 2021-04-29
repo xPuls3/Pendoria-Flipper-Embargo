577e2cbcffdd862f2cd49d76c2c12c1f18f1494e
xPuls3 <Xer0.Puls3.XXII@gmail.com>
2021-04-29 13:36:38 -0400

    Add files via upload

---
+++ script.user.js
@@ -0,0 +1,72 @@
+// ==UserScript==
+// @name Pendoria Flipper Embargo
+// @namespace http://pendoria.net/
+// @version 1.0
+// @author Puls3
+// @include /^https?:\/\/(?:.+\.)?pendoria\.net\/?(?:.+)?$/
+// @homepage https://github.com/xPuls3/Pendoria-Flipper-Embargo
+// @supportURL https://github.com/xPuls3/Pendoria-Flipper-Embargo/issues
+// @downloadURL https://github.com/xPuls3/Pendoria-Flipper-Embargo/raw/master/script.user.js
+// @updateURL https://github.com/xPuls3/Pendoria-Flipper-Embargo/raw/master/script.user.js
+// @icon https://raw.githubusercontent.com/xPuls3/Pendorian-Elite-UI/master/favicon.ico
+// @grant none
+// @run-at document-start
+// @description Changes a large portion of the text and UI to 'Elite' blue and makes small changes to the UI.
+// ==/UserScript==
+
+// Pendoria Flipper Embargo is only officially distributed on GitHub!
+// - https://github.com/xPuls3/Pendoria-Flipper-Embargo/
+
+// This script was created by Puls3!
+// - Puls3 on Pendoria
+
+let originalAjaxPost = null;
+let flippers = null;
+
+window.addEventListener("DOMContentLoaded", () => {
+    initiate();
+})
+
+function initiate() {
+
+    originalAjaxPost = window["ajaxPost"];
+
+    const styleElement = document.createElement("style");
+    styleElement.innerText = ".dirty-flipper { color: yellow !important; }"
+
+    document.head.append(styleElement)
+
+    window["jQuery"].get("https://raw.githubusercontent.com/xPuls3/pendoria-flipper-registry/main/list.json", (data) => {
+
+        console.log("Loaded flippers from remote registry.");
+
+        flippers = JSON.parse(data);
+        window["ajaxPost"] = newAjaxPost;
+
+    })
+
+}
+
+function newAjaxPost(...args) {
+
+    if (!args[0].startsWith("/market/")) originalAjaxPost(...args);
+
+    originalAjaxPost(args[0], (data) => {
+
+        if (!(typeof data === "string")) return args[1](data);
+
+        let newData = data;
+
+        for (let i = 0; i <= flippers.length; i++) {
+
+            if (i === flippers.length) return args[1](newData);
+
+            const flipperId = String(flippers[i].id);
+            const regex = new RegExp(`data-player-id=\"${flipperId}\">`, "gi");
+            newData = newData.replace(regex, `data-player-id=\"${flipperId}\" class=\"dirty-flipper\"`)
+
+        }
+
+    })
+
+}
